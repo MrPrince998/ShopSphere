2fe558a45ea276c03d01ed27a60c184222abd0c7
Prince rana <peincerana234@gmail.com>
2025-11-17 18:45:01 +0545

    added: vercel json for route

--- ShopSphere/src/lib/api/auth.ts
+++ ShopSphere/src/lib/api/auth.ts
@@ -3,18 +3,19 @@ import type {
   RegisterCredentials,
 } from "@/components/const/types";
 import { api } from "../axios";
+import { BACKENDURL } from "@/components/const/const";
 
 export const login = async (data: LoginCredentials) => {
-  const response = await api.post("/auth/login", data);
+  const response = await api.post(`${BACKENDURL}/auth/login`, data);
   return response.data;
 };
 
 export const logout = async () => {
-  const response = await api.post("/auth/logout");
+  const response = await api.post(`${BACKENDURL}/auth/logout`);
   return response.data;
 };
 
 export const register = async (data: RegisterCredentials) => {
-  const response = await api.post("/auth/register", data);
+  const response = await api.post(`${BACKENDURL}/auth/register`, data);
   return response.data;
 };
--- ShopSphere/src/lib/api/users.ts
+++ ShopSphere/src/lib/api/users.ts
@@ -1,21 +1,22 @@
+import { BACKENDURL } from "@/components/const/const";
 import { api } from "../axios";
 
 export const getUsers = async () => {
-  const response = await api.get("/users");
+  const response = await api.get(`${BACKENDURL}/users`);
   return response.data;
 };
 
 export const getUserById = async (id: string) => {
-  const response = await api.get(`/users/${id}`);
+  const response = await api.get(`${BACKENDURL}/users/${id}`);
   return response.data;
 };
 
 export const updateUser = async (id: string, data: any) => {
-  const response = await api.put(`/users/${id}`, data);
+  const response = await api.put(`${BACKENDURL}/users/${id}`, data);
   return response.data;
 };
 
 export const deleteUser = async (id: string) => {
-  const response = await api.delete(`/users/${id}`);
+  const response = await api.delete(`${BACKENDURL}/users/${id}`);
   return response.data;
 };
